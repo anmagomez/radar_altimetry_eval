b446a05a26bb3dbda68d148972dec6e6bf839496
Angelica <amgomez@live.unc.edu>
2025-05-01 10:46:40 -0500

    Change to format of new data

--- src/ground_data.py
+++ src/ground_data.py
@@ -9,18 +9,18 @@ class GroundObservations:
         #Initialize all the path variables. Change when services can be stablished directly 
         self.usgs_path='../data/source_in_situ/USGS_data_gages.csv'
         self.arhn_path='../data/source_in_situ/sel_argentina/'
-        self.locss_r_path='../data/source_in_situ/readings_up_to_20220511.csv'
-        self.locss_g_path='../data/source_in_situ/gauges_up_to_20220404.csv'
+        self.locss_r_path='../data/source_in_situ/lake_heights_all_2017-01-01_2025-04-28.csv'#'../data/source_in_situ/readings_up_to_20220511.csv'
+        self.locss_g_path='../data/source_in_situ/gauges_list.csv'#'../data/source_in_situ/gauges_up_to_20220404.csv'
         self.rvbr_path='../data/source_in_situ/brasil/'
     
-    def read_ground_data(self, source, file_type='.csv', date_fd=None, time_fd=None,
+    def read_ground_data(self, source, file_type='.csv', date_fd=None, 
                          id_fd=None, height_fd=None, station_id=None, path=None, skip_rows=0):
         
         #if source=='USGS':
             #Create function to connect to R and get the station
         df=None
         if source=='LOCSS':
-            df=self.get_locss(source,date_fd, time_fd,id_fd, height_fd, station_id)
+            df=self.get_locss(source,date_fd, id_fd, height_fd, station_id)
             
         if source=='ARHN':
             df=self.get_arhn(source, date_fd, id_fd, height_fd, station_id, path, skip_rows)
@@ -155,45 +155,111 @@ class GroundObservations:
         df[date_fd]=pd.to_datetime(df[date_fd], dayfirst=True)
         return df
         
-    def get_locss(self,source, date_fd=None, time_fd=None,id_fd=None, height_fd=None, station_id=None, all_fds=False):
+    def get_locss(self,source, date_fd=None, id_fd=None, height_fd=None, station_id=None, all_fds=False):
         #Tested with source only rest values by default
         #For now the data is saved in a fix rout, this has to change later when I get the API
-        dir_ts=self.locss_r_path
-        dir_loc=self.locss_g_path
+        dir_ts=self.locss_r_path#'../data/source_in_situ/NewReview_TS_LOCSS_2G_Gauge/lake_heights_FDN2_2017-01-01_2025-04-28.csv'
+        dir_loc=self.locss_g_path#'../data/source_in_situ/NewReview_TS_LOCSS_2G_Gauge/gauges_list.csv'
         if date_fd is None: #Tested
             date_fd='date'
-            time_fd='time'
+            #time_fd='time'
         if id_fd is None: #Tested
             id_fd='gauge_id' 
         if height_fd is None: #Tested
             height_fd='height'
         #Load the time series
         df_locss=pd.read_csv(dir_ts, parse_dates=[date_fd])
-
+    
         #Read the coordinates
-        df_coord_locss=pd.read_csv(dir_loc, sep=",")
-        df_coord_locss['location']=df_coord_locss[id_fd].str.slice(2,4)
-
+        df_gauges=pd.read_csv(dir_loc, sep=",")
+        if station_id is not None:
+            if isinstance(station_id, list):
+                
+                id_list=df_gauges.loc[df_gauges[id_fd].isin(station_id), id_fd].unique()
+                # print(id_list)
+            else:
+                print('Must be a list')
+        else: #Take all the gauges        
+            id_list=df_gauges[id_fd].unique()
+        # print(id_list)
+        df_fo_readings=pd.DataFrame()
+        for d_id in id_list:
+            df=df_locss.loc[df_locss[id_fd]==d_id].copy()
+            # print(d_id, 'here')
+           
+            if not df.empty:
+                time_zone=df_gauges.loc[df_gauges[id_fd]==d_id, 'timezone'].values[0]
+                min_height=df_gauges.loc[df_gauges[id_fd]==d_id, 'min_height'].values[0]
+                max_height=df_gauges.loc[df_gauges[id_fd]==d_id, 'max_height'].values[0]
+                gauge_id=df_gauges.loc[df_gauges[id_fd]==d_id,'gauge_id'].values[0]
+                unit=df_gauges.loc[df_gauges[id_fd]==d_id, 'unit'].values[0]
+                name=df_gauges.loc[df_gauges[id_fd]==d_id, 'name'].values[0]
+                lat=df_gauges.loc[df_gauges[id_fd]==d_id, 'latitude'].values[0]
+                long=df_gauges.loc[df_gauges[id_fd]==d_id, 'longitude'].values[0]
+                
+                df['datetime_utc'] = pd.to_datetime(df[date_fd], format='%Y-%m-%d HH:MM:SS')#.dt.strftime('%Y-%m-%d')+' '+df['timeUTC']
+                df['min_height']=min_height
+                df['max_height']=max_height
+                df['gauge_id']=gauge_id
+                df['unit']=unit
+                df['name']=name
+                df['latitude']=lat
+                df['longitude']=long
+                # print(df.head())
+                df_fo_readings=pd.concat([df_fo_readings, df])
+        #df_coord_locss['location']=df_coord_locss[id_fd].str.slice(2,4)
+    
         gc=GaugeCollection()
-        df_locss_filtered=gc.filter_test_gages(df_locss,id_fd).copy()
-        #Convert field to datetime
-        df_locss_filtered[date_fd]=pd.to_datetime((df_locss_filtered[date_fd].astype(str)+' '+df_locss_filtered[time_fd].astype(str)), 
-                                                  format='%Y-%m-%d %H:%M:%S')
-        if all_fds==False:
-            df_locss_filtered=pd.merge(df_locss_filtered, df_coord_locss[[id_fd,'min_height','max_height', 'unit']], on=id_fd)
-        else:
-            df_locss_filtered=pd.merge(df_locss_filtered, df_coord_locss, on=id_fd)
+        df_locss_filtered=gc.filter_test_gages(df_fo_readings,id_fd).copy()
+        # #Convert field to datetime
+        # df_locss_filtered[date_fd]=pd.to_datetime((df_locss_filtered[date_fd].astype(str)+' '+df_locss_filtered[time_fd].astype(str)), 
+        #                                           format='%Y-%m-%d %H:%M:%S')
+        # if all_fds==False:
+        #     df_locss_filtered=pd.merge(df_locss_filtered, df_coord_locss[[id_fd,'min_height','max_height', 'unit']], on=id_fd)
+        # else:
+        #     df_locss_filtered=pd.merge(df_locss_filtered, df_coord_locss, on=id_fd)
         df_locss_filtered=df_locss_filtered.loc[(df_locss_filtered[height_fd]>=df_locss_filtered['min_height'])&
                                                 (df_locss_filtered[height_fd]<=df_locss_filtered['max_height'])].copy()
         df_locss_filtered['source']=source
-        if station_id is not None:
-            if type(station_id)==list:
-                df_locss_filtered=df_locss_filtered.loc[df_locss_filtered[id_fd].isin(station_id)]
-            else:
-                df_locss_filtered=df_locss_filtered.loc[df_locss_filtered[id_fd]==station_id]
-        
+       
         df_locss_filtered=self._unify_cols(df_locss_filtered, id_fd, date_fd, height_fd)
         return df_locss_filtered
+        # dir_ts=self.locss_r_path
+        # dir_loc=self.locss_g_path
+        # if date_fd is None: #Tested
+        #     date_fd='date'
+        #     time_fd='time'
+        # if id_fd is None: #Tested
+        #     id_fd='gauge_id' 
+        # if height_fd is None: #Tested
+        #     height_fd='height'
+        # #Load the time series
+        # df_locss=pd.read_csv(dir_ts, parse_dates=[date_fd])
+
+        # #Read the coordinates
+        # df_coord_locss=pd.read_csv(dir_loc, sep=",")
+        # df_coord_locss['location']=df_coord_locss[id_fd].str.slice(2,4)
+
+        # gc=GaugeCollection()
+        # df_locss_filtered=gc.filter_test_gages(df_locss,id_fd).copy()
+        # #Convert field to datetime
+        # df_locss_filtered[date_fd]=pd.to_datetime((df_locss_filtered[date_fd].astype(str)+' '+df_locss_filtered[time_fd].astype(str)), 
+        #                                           format='%Y-%m-%d %H:%M:%S')
+        # if all_fds==False:
+        #     df_locss_filtered=pd.merge(df_locss_filtered, df_coord_locss[[id_fd,'min_height','max_height', 'unit']], on=id_fd)
+        # else:
+        #     df_locss_filtered=pd.merge(df_locss_filtered, df_coord_locss, on=id_fd)
+        # df_locss_filtered=df_locss_filtered.loc[(df_locss_filtered[height_fd]>=df_locss_filtered['min_height'])&
+        #                                         (df_locss_filtered[height_fd]<=df_locss_filtered['max_height'])].copy()
+        # df_locss_filtered['source']=source
+        # if station_id is not None:
+        #     if type(station_id)==list:
+        #         df_locss_filtered=df_locss_filtered.loc[df_locss_filtered[id_fd].isin(station_id)]
+        #     else:
+        #         df_locss_filtered=df_locss_filtered.loc[df_locss_filtered[id_fd]==station_id]
+        
+        # df_locss_filtered=self._unify_cols(df_locss_filtered, id_fd, date_fd, height_fd)
+        # return df_locss_filtered
             
         
     def _unify_cols(self,df, id_fd,date_fd,height_fd, filter_others_out=False):
